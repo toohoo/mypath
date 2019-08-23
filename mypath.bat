d3e891ec0c726bad5927a590c2ed7e6410f50f4b
Thomas Hofmann <toohoo@gmail.com>
2019-08-23 12:17:03 +0200

    small corrections, call perl.exe directly

--- mypath.bat
+++ mypath.bat
@@ -1,11 +1,16 @@
 @echo off
 @if exist d:\temp\mypath.pl goto dmypath
-REM @if exist z:\temp\zmypath.pl goto zmypath
+@if exist c:\temp\mypath.pl goto cmypath
 goto mypathbatende
-REM :zmypath
-REM call perl z:\temp\zmypath.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
-REM goto mypathbatende
+REM perl.exe must be found in PATH already, otherwise add the folder to PATH
+REM If you have more than 4 folder actions be aware to call the perl.exe command yourself on command line
+REM   DOS-Box/Windows-command line can't handle more parameters than %9
+:cmypath
+perl.exe c:\temp\mypath.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+call mypathnew
+goto mypathbatende
 :dmypath
-call perl d:\temp\mypath.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+perl.exe d:\temp\mypath.pl %1 %2 %3 %4 %5 %6 %7 %8 %9
+call mypathnew
 goto mypathbatende
 :mypathbatende
